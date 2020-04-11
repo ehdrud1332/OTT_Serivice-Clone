23f281d27ec8cb555b2d2a8c0610c9de992a12ef
bob <ehdrud1332@gmail.com>
2020-04-11 11:39:57 +0900

    CSS in React

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 import Router from './Router';
-import Header from './Header';
 
 
 class App extends Component {
     render() {
         return (
             <div>
-                <Header/>
                 <Router />
             </div>
         );
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,14 +3,19 @@ import { HashRouter as Router, Route, Redirect, Switch } from "react-router-dom"
 import Home from "../Router/Home";
 import Search from "../Router/Search";
 import TV from "../Router/TV";
+import Header from "./Header";
+
 
 export default () => (
     <Router>
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/tv" exact component={TV} />
-            <Route path="/search" exact component={Search} />
-            <Redirect from="*" to="/" />
-        </Switch>
+        <>
+            <Header/>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/tv" exact component={TV} />
+                <Route path="/search" exact component={Search} />
+                <Redirect from="*" to="/" />
+            </Switch>
+        </>
     </Router>
 )

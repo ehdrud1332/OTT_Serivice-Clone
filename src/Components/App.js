ec1ea44b04a8561c761957af0a77cefd43df240e
bob <ehdrud1332@gmail.com>
2020-04-11 12:04:05 +0900

    Globalstyles and Header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Router from './Router';
+import globalStyles from '../Components/GlobalStyles';
 
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
         return (
             <div>
                 <Router />
+                <globalStyles/>
             </div>
         );
     }

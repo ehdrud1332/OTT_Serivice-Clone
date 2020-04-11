4d182164d411cff9e6a601251f4db7a8536d1357
bob <ehdrud1332@gmail.com>
2020-04-11 12:42:01 +0900

    API Setting #2

--- src/api.js
+++ src/api.js
@@ -11,11 +11,39 @@ const api = axios.create({
 export const moviesApi = {
     nowPlaying: () => api.get("movie/now_playing"),
     upcoming: () => api.get("movie/upcoming"),
-    popular: () => api.get("movie/popular")
+    popular: () => api.get("movie/popular"),
+    movieDetail: id =>
+        api.get(`movie/${id}`, {
+            params: {
+                append_to_response: "videos"
+            }
+        }),
+    search: keyword => {
+        api.get("search/movie", {
+            params: {
+                query: encodeURIComponent(keyword)
+            }
+        })
+    }
+
 };
 
 export const tvApi = {
     topRated: () => api.get("tv/top_rated"),
     popular: () => api.get("tv/popular"),
     airingToday: () => api.get("tv/airing_today"),
+    tvDetail: id =>
+        api.get(`tv/${id}`, {
+            params: {
+                append_to_response: "videos"
+            }
+        }),
+    search: keyword => {
+        api.get("search/tv", {
+            params: {
+                //검색어
+                query: encodeURIComponent(keyword)
+            }
+        })
+    }
 };
